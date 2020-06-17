724e2a37ada3346dd00f4673f7415a117219d52c
S-X-J <normansehn@gmail.com>
2020-06-17 11:51:34 +0800

    first commit

---
+++ src/main/java/com/mogu/pay/PayCenterApplication.java
@@ -0,0 +1,13 @@
+package com.mogu.pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mogu/pay/PayCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mogu.pay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
